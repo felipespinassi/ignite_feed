6c69a24329433c8461eef2ee3ba214aa18f0145d
felipespinassi <felipe_spinassi@hotmail.com>
2022-09-25 20:42:30 -0300

    estilizando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from "./components/Header/Header";
-import "./styles.css";
+import "./global.css";
 export function App() {
   return <Header />;
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,10 @@
 import styles from "./Header.module.css";
+import igniteLogo from "../../assets/ignite-logo.svg";
+
 export function Header() {
-  return <h1 className={styles.header}>header</h1>;
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="" />
+    </header>
+  );
 }
